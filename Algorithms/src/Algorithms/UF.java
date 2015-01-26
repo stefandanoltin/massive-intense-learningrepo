7c70d762ebbcad65bc6a716275e5f4c44ca73210
dastefan <dastefan@BUCD336.misys.global.ad>
2015-01-26 18:06:22 +0200

    unions

---
+++ Algorithms/src/Algorithms/UF.java
@@ -0,0 +1,24 @@
+package Algorithms;
+
+
+
+public class UF {
+	UF(int N) {
+	};
+
+	void union(int p, int q) {
+	}
+
+	boolean connected(int p, int q) {
+		return false;
+	}
+
+	int find(int p) {
+		return 0;
+	}
+
+	int count() {
+		return 0;
+	}
+
+}
